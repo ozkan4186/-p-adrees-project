db96a24942d8ab585618fd83cb8534903b229731
özkan demir <ozkanndemir2009@gmail.com>
2022-09-30 03:27:37 +0300

    java added

---
+++ app.js
@@ -0,0 +1,60 @@
+const btn = document.querySelector("#search-btn");
+const input = document.querySelector("#ip_adress");
+const map = document.querySelector(".map")
+
+
+const ipp = document.querySelector("#current_ip");
+const town = document.querySelector("#current_town");
+const zone = document.querySelector("#current_zone");
+const ısp = document.querySelector("#current_Isp");
+
+
+
+
+const getIp = async () => {
+    const url = `https://geo.ipify.org/api/v2/country?apiKey=at_eFWbOEoIwMNa36i36Q8uhdp5RH5Nl&ipAddress=` + input.value;
+
+
+    try {
+        const res = await fetch(url);
+
+        if (!res.ok) {
+            alert("ikazzzzz!!!!")
+            throw new Error("ikazz")
+        }
+        //? ****data'yı çektik
+        const data = await res.json()
+        console.log(data)
+        const {
+            ip,
+            location: {
+                timezone
+            },
+            location: {
+                region
+            },
+            location: {
+                country
+            },
+            isp,
+        } = data;
+
+        ipp.innerHTML = `${ip}`;
+        town.innerHTML = `${country},${region}`;
+        zone.innerHTML = `UTC${timezone}`;
+        ısp.innerHTML = `${isp}`
+
+
+    } catch (err) {
+        console.log(err)
+    }
+
+
+}
+// getIp();
+btn.addEventListener("click", (e) => {
+    console.log(e.target)
+    e.preventDefault()
+    getIp()
+
+})
